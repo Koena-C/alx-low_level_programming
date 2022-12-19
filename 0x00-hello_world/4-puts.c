552ac51b2ccbc58ea7383a48810f4c357dd7aaa2
Koena <kowena780@gmail.com>
2022-12-19 01:00:57 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+
+}       
