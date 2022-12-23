b9fe4ab92cd5d5b201b92bac00df8ab95e715818
Koena <kowena780@gmail.com>
2022-12-23 05:20:17 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,6 @@
 /**
  * function that takes a pointer to an integer 98
  *
- * reset_to_98: Function that takes a pointer to an integer param
- *
  * @n: integer pointer
  *
  * Description: reset_to_98 - function that takes a pointer to an int
