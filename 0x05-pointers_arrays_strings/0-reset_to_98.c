1b64b64a6c1fc17e19a221b688488ddbf9b7c7d3
Koena <kowena780@gmail.com>
2022-12-23 03:58:29 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,7 @@
  *
  * @n: integer pointer
  *
- * reset_to_98 - function that takes a pointer to an int
+ * Description: reset_to_98 - function that takes a pointer to an int
  *
  * Return: Nothing
  */
