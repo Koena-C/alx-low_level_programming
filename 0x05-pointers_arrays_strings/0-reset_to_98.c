df401b5210dc511b6af6c955bb5c7f1619f73ef2
Koena <kowena780@gmail.com>
2022-12-23 05:23:43 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
- * function that takes a pointer to an integer 98
- *
+ * reset_to_98 - function that takes a pointer to an interger parameter
  * @n: integer pointer
- *
- * Description: reset_to_98 - function that takes a pointer to an int
- *
  * Return: Nothing
  */
 
